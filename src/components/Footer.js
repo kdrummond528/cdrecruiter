bc64289c0acc7d1dc64287e2bd96c8dcc53e8a71
Drummond Mac <Drummond@drummond-imac.mynetworksettings.com>
2024-01-24 22:58:54 -0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+
+            <footer>
+                &copy; 2024 Chris Drummond
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer;
