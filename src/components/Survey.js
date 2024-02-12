9799435ba353d5c88d43237ca30696bd6c31e037
Karina Drummond <karinadrummond@katies-imac.lan>
2024-02-11 18:03:00 -0800

    modified components

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -4,8 +4,8 @@ const Survey = () => {
     return (
         <div>
 
-            <div className='survey'>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf9lBeS5ZCcyeE-EQRDMe0ye4N2tbMPFgtBbNg-roLwlIwXEw/viewform?embedded=true" width="640" height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <div>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf9lBeS5ZCcyeE-EQRDMe0ye4N2tbMPFgtBbNg-roLwlIwXEw/viewform?embedded=true" width="640" height="650" frameborder="0" marginheight="0" className='survey'marginwidth="0">Loading…</iframe>
             </div>
 
         </div>
