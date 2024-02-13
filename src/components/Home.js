772d23b7df86aab77d8aa620aa6014e1e6317c63
Karina Drummond <karinadrummond@katies-imac.lan>
2024-02-13 13:42:54 -0800

    modified components, css, completed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,43 @@
 import React from 'react';
-// import Ntag from '../img/ntag_logo.PNG';
-// import { Image } from '@chakra-ui/react';
+import Ntag from '../img/ntag_logo.PNG';
+import { Card, Image, Stack, Heading, Text, Button, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
 
 const Home = () => {
     return (
         <div>
 
-            {/* <Image borderRadius='full'
-                boxSize='300px' className='ntag-logo' src={Ntag} align='left' /> */}
+            <Card className='card'
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+            >
 
-            <div className='home-div'>
-                <p className='pgheader'>Home</p>
-                <div className='body-div'>
+                <Image className='ntag-logo'
+                    objectFit='cover'
+                    maxW={{ base: '100%', sm: '350px' }}
+                    src={Ntag} alt='ntag logo' />
 
-                    <p className='body'>
-                        This is the home page.<br />
-                        <br />
-                        Enter some intro content here.
-                    </p>
-                </div>
-            </div>
+                <Stack>
+                    <CardBody>
+                        <Heading size='md'>Welcome Aboard!</Heading>
+
+                        <Text py='2' textAlign='left'>
+                            Congratulations on taking the first step of learning about what your new life could be with a future in the United States Navy, and exploring options that are available to you as a potential Future Sailor.<br /><br />
+                            Think of me as your guide and all things Navy connoisseur while you begin this journey to change your life and build on your future.<br /><br />
+                            Use the links in the navigation bar to learn about me, the Navy, send me your contact information and schedule an appointment.
+                            <br /><br />
+                            <i>"Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek." — Barack Obama</i>
+                        </Text>
+                    </CardBody>
+
+                    {/* <CardFooter>
+                        <p>
+                            <i>"Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek." — Barack Obama</i>
+                        </p>
+                    </CardFooter> */}
+
+                </Stack>
+            </Card>
 
         </div>
     )
