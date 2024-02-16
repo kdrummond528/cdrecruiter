5aecda57e6051841175ff2df4188eac516a40a30
Karina Drummond <karinadrummond@katies-imac.lan>
2024-02-16 00:17:47 -0800

    modified components

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -16,87 +16,87 @@ const Gallery = () => {
             <p className='pgheader'>Gallery</p>
             <p className='pg-subheading'>Some accomplishments and highlights of my career in the Navy.</p>
             <br />
-            
+
             <div className='gallery'>
-                <Stack direction='row'>
+                {/* <Stack direction='row'> */}
 
-                    <Card className='gallery-card'>
-                        <CardBody>
-                            <Image className='gallery-img'
-                                boxSize='200px'
-                                objectFit='cover'
-                                src={EngineRoom}
-                                alt='Chris Drummond'
-                            />
-                            <p className='gallery-name'>
-                                Emergent Repair
-                            </p>
-                            <Text className='gallery-text'>Summary of the story behind the picture.</Text>
-                            <p className='gallery-year'>USS Cincinnati 2022</p>
-                        </CardBody>
-                    </Card>
+                <Card className='gallery-card'>
+                    <CardBody>
+                        <Image className='gallery-img'
+                            boxSize='200px'
+                            objectFit='cover'
+                            src={EngineRoom}
+                            alt='Chris Drummond'
+                        />
+                        <p className='gallery-name'>
+                            Emergent Repair
+                        </p>
+                        <Text className='gallery-text'>Summary of the story behind the picture.</Text>
+                        <p className='gallery-year'>USS Cincinnati 2022</p>
+                    </CardBody>
+                </Card>
 
-                    <Card className='gallery-card'>
-                        <CardBody>
-                            <Image className='gallery-img'
-                                boxSize='200px'
-                                objectFit='cover'
-                                src={NoruGrad}
-                                alt='Chris Drummond'
-                            />
-                            <p className='gallery-name'>
-                                NORU Graduation
-                            </p>
-                            <Text className='gallery-text'>Summary of the story behind the picture.</Text>
-                            <p className='gallery-year'>2023</p>
-                        </CardBody>
-                    </Card>
+                <Card className='gallery-card'>
+                    <CardBody>
+                        <Image className='gallery-img'
+                            boxSize='200px'
+                            objectFit='cover'
+                            src={NoruGrad}
+                            alt='Chris Drummond'
+                        />
+                        <p className='gallery-name'>
+                            NORU Graduation
+                        </p>
+                        <Text className='gallery-text'>Summary of the story behind the picture.</Text>
+                        <p className='gallery-year'>NORU 2023</p>
+                    </CardBody>
+                </Card>
 
-                    <Card className='gallery-card'>
-                        <CardBody>
-                            <Image className='gallery-img'
-                                boxSize='200px'
-                                src={Promotion}
-                                objectFit='cover'
-                                alt='Chris Drummond' />
-                            <p className='gallery-name'>
-                                EN1 Promotion
-                            </p>
-                            <Text className='gallery-text'>Summary of the story behind the picture.</Text>
-                            <p className='gallery-year'>USS Cincinnati 2023</p>
-                        </CardBody>
-                    </Card>
+                <Card className='gallery-card'>
+                    <CardBody>
+                        <Image className='gallery-img'
+                            boxSize='200px'
+                            src={Promotion}
+                            objectFit='cover'
+                            alt='Chris Drummond' />
+                        <p className='gallery-name'>
+                            EN1 Promotion
+                        </p>
+                        <Text className='gallery-text'>Summary of the story behind the picture.</Text>
+                        <p className='gallery-year'>USS Cincinnati 2023</p>
+                    </CardBody>
+                </Card>
 
-                    <Card className='gallery-card'>
-                        <CardBody>
-                            <Image className='gallery-img'
-                                boxSize='200px'
-                                src={FleetWk}
-                                objectFit='cover'
-                                alt='Chris Drummond' />
-                            <p className='gallery-name'>
-                                Fleet Week
-                            </p>
-                            <Text className='gallery-text'>Summary of the story behind the picture.</Text>
-                            <p className='gallery-year'>USS Cincinnati 2023</p>
-                        </CardBody>
-                    </Card>
+                <Card className='gallery-card'>
+                    <CardBody>
+                        <Image className='gallery-img'
+                            boxSize='200px'
+                            src={FleetWk}
+                            objectFit='cover'
+                            alt='Chris Drummond' />
+                        <p className='gallery-name'>
+                            Fleet Week
+                        </p>
+                        <Text className='gallery-text'>Summary of the story behind the picture.</Text>
+                        <p className='gallery-year'>USS Cincinnati 2023</p>
+                    </CardBody>
+                </Card>
 
-                    <Card className='gallery-card'>
-                        <CardBody>
-                            <Image className='gallery-img'
-                                boxSize='200px'
-                                src={RibPic}
-                                objectFit='cover'
-                                alt='Chris Drummond' />
-                            <p className='gallery-name'>
-                                Boat Ops</p>
-                            <Text className='gallery-text'>Summary of the story behind the picture.</Text>
-                            <p className='gallery-year'>USS Cincinnati 2023</p>
-                        </CardBody>
-                    </Card>
+                <Card className='gallery-card'>
+                    <CardBody>
+                        <Image className='gallery-img'
+                            boxSize='200px'
+                            src={RibPic}
+                            objectFit='cover'
+                            alt='Chris Drummond' />
+                        <p className='gallery-name'>
+                            Boat Ops</p>
+                        <Text className='gallery-text'>Summary of the story behind the picture.</Text>
+                        <p className='gallery-year'>USS Cincinnati 2023</p>
+                    </CardBody>
+                </Card>
 
-                </Stack>
+                {/* </Stack> */}
 
             </div>
         </div>
