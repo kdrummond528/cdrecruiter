6cd910bce076ae3b49c7fe0657480d056c20245f
Karina Drummond <karinadrummond@Katies-iMac.local>
2024-02-22 14:19:37 -0800

    modified benefits component

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Accordion, Box, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
+import EdForLife from './doc/Benefits/EdforLife.pdf';
+import GI from './doc/Benefits/GIFAQ.pdf';
+import ParentalLeave from './doc/Benefits/ParentalLeave.pdf';
+import Childcare from './doc/Benefits/Childcare.pdf';
 
 const Benefits = () => {
     return (
@@ -9,6 +13,8 @@ const Benefits = () => {
                 Explore the various benefits that would become available to you by joining the United States Navy.
             </p>
             <Accordion allowToggle className='card'>
+
+                {/* Medical */}
                 <AccordionItem>
                     <h2>
                         <AccordionButton>
@@ -24,6 +30,7 @@ const Benefits = () => {
                     </AccordionPanel>
                 </AccordionItem>
 
+                {/* Education */}
                 <AccordionItem>
                     <h2>
                         <AccordionButton>
@@ -34,11 +41,25 @@ const Benefits = () => {
                         </AccordionButton>
                     </h2>
                     <AccordionPanel pb={4} textAlign='left'>
-                        Enter education benefits here...<br />
-                        TA, GI, JST, Loan Repayment
+                        Joining the Navy offers many opportunities to enhance your education.
+                        Some of the education programs include: <br /><br />
+                        <li>The United Services Military Apprenticeship Program (<i>USMAP</i>)</li>
+                        <li>Tuition Assistance (<i>TA</i>)</li>
+                        <li>Academic Credit for Navy Training and Work — Joint Service Transcript (<i>JST</i>)</li>
+                        <li>Post 9/11 Forever GI Bill</li>
+                        <li>College Entrance Testing Program</li>
+                        <li>The Navy College Program</li>
+                        <li>The College First Program</li>
+                        <li>Loan Repayment</li>
+                        {/* <li>Nuclear Propulsion Officer Candidate Program (<i>NUPOC</i>)</li>
+                        <li> (<i></i>)</li> */}
+
+                        <br />
+                        View the <a target='_blank' className='link' href={EdForLife}>Education for Life</a> guide and the <a target='_blank' className='link' href={GI}>Forever GI Bill FAQ</a> sheet for more information on these programs.
                     </AccordionPanel>
                 </AccordionItem>
 
+                {/* OJT */}
                 <AccordionItem>
                     <h2>
                         <AccordionButton>
@@ -54,11 +75,12 @@ const Benefits = () => {
                     </AccordionPanel>
                 </AccordionItem>
 
+                {/* Housing Allowance & VA Loan */}
                 <AccordionItem>
                     <h2>
                         <AccordionButton>
                             <Box as="span" className='title' flex='1' textAlign='left'>
-                                Housing
+                                Housing Allowance & VA Loan
                             </Box>
                             <AccordionIcon />
                         </AccordionButton>
@@ -66,6 +88,24 @@ const Benefits = () => {
                     <AccordionPanel pb={4} textAlign='left'>
                         Enter housing benefits here...<br />
                         VA loan, BAH, etc...
+                    </AccordionPanel>
+                </AccordionItem>
+
+                {/* Parental Leave & Childcare */}
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" className='title' flex='1' textAlign='left'>
+                                Parental Leave & Childcare
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4} textAlign='left'>
+
+                        The Navy offers maternity convalescent leave, and the primary or secondary caregiver a non-chargeable leave period after the birth of a child. View the <a target='_blank' className='link' href={ParentalLeave}>Parental Leave FAQ</a> sheet for more information.
+                        <br /><br />
+                        Childcare through the Navy offers affordable parent fees based on total family income. For more information, view the  <a target='_blank' className='link' href={Childcare}>Childcare FAQ</a> sheet.
                     </AccordionPanel>
                 </AccordionItem>
 
